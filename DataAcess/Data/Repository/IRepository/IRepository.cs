300b1febd37cb1a52934baeb3474fcfb19d9e8fb
BRENO-MORAIS <brenofranciscomorais@gmail.com>
2021-01-26 07:14:56 -0400

    Repository
    Implementação de Expressão.

--- DataAcess/Data/Repository/IRepository/IRepository.cs
+++ DataAcess/Data/Repository/IRepository/IRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace DataAcess
 {
@@ -10,5 +12,6 @@ namespace DataAcess
          T GetId(int i);
          void Update(T entity);
          void Save();
+         IEnumerable<T> GetAll(Expression<Func<T, bool>> a);
     }
 }
